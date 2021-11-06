76b6e1f527cb663b3543b883ae86c410cf275ed2
Xuan Tang <txstc55@gmail.com>
2021-11-07 00:22:19 +0800

    add /

--- draw-rectangles-together-client/vue.config.js
+++ draw-rectangles-together-client/vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   transpileDependencies: ["vuetify"],
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/draw-rectangles-together/draw-rectangles-together-client'
+    ? '/draw-rectangles-together/draw-rectangles-together-client/'
     : '/'
 };
